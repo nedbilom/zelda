2fada407fd1148ba6adee25736bc38986545662a
denis stan <molibden992@gmail.com>
2024-04-17 17:58:01 +0700

    добавил поле 'статус' к таблицам игры и коммента

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_12_032134) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_17_105156) do
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
     t.integer "game_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
     t.index ["game_id"], name: "index_comments_on_game_id"
   end
 
@@ -25,6 +26,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_12_032134) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
   add_foreign_key "comments", "games"
